6a331d9ac0da657d7aeaebe576eb91202a9c8bd3
SeanFlannery <sflanner@purdue.edu>
2018-07-21 02:40:24 -0400

    Modified Math Utility methods and added comments

--- src/MathUtils.java
+++ src/MathUtils.java
@@ -3,8 +3,8 @@ public class MathUtils {
      * divideNumbers -- a / b
      * Divide a by b and return the result
      */
-    public float divideNumbers(int a, int b) {
-        float res = a / b;
+    public static double divideNumbers(int dividend, int divisor) {
+        double res = dividend / divisor;
         return res;
     }
 
@@ -12,13 +12,21 @@ public class MathUtils {
      * raiseToPower -- (base)^(exp)
      * Raise the number base to power exp
      */
-    public float raiseToPower(int base, int exp) {
+    public static double raiseToPower(int base, int exp) {
         int res = base;
+        boolean isNegative = false;
+
+        if (exp < 0) {
+            exp *= -1;
+            isNegative = true;
+        }
+
         for (int i = 0; i < exp; i++) {
            res *= base; 
         }
+
         // If it's a negative exponent invert it
-        if (exp < 0) {
+        if (isNegative) {
             return divideNumbers(1, res);
         } else {
             return res;
