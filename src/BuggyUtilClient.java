e71e25a49929a2a8a48cd58906807733b71162f3
SeanFlannery <sflanner@purdue.edu>
2018-09-18 13:08:57 -0400

    Add manual test of StringUtils in main

--- src/BuggyUtilClient.java
+++ src/BuggyUtilClient.java
@@ -1,8 +1,12 @@
 
 public class BuggyUtilClient {
     public static void main(String[] args) {
-
-
+        // First test case in Main
+        System.out.println("Example 1: Make My Username");
+        System.out.println("In: first = " + "\"Sean\"" +
+                ", last = " + "\"FLnanery\"");
+        System.out.println("Out: " +
+        StringUtils.makePurdueUsername("Sean", "Flannery"));
     }
 }
 
