2f7f68213549c456ffcb7ff82dd5dbf6b031ea1d
Sean Flannery <sflanner@purdue.edu>
2018-09-18 15:27:02 -0400

    Fix my own name lol

--- src/BuggyUtilClient.java
+++ src/BuggyUtilClient.java
@@ -4,7 +4,7 @@ public class BuggyUtilClient {
         // First test case in Main
         System.out.println("Example 1: Make My Username");
         System.out.println("In: first = " + "\"Sean\"" +
-                ", last = " + "\"FLnanery\"");
+                ", last = " + "\"FLannery\"");
         System.out.println("Out: " +
         StringUtils.makePurdueUsername("Sean", "Flannery"));
     }
